31f4655128b302df3df9b039647c5e427e9ab46a
Chavy1322303 <chavi132.2303@gmail.com>
2025-05-28 18:58:51 -0400

    fix(ambiente): eliminar rutas extras

--- src/routes/ambiente.routes.ts
+++ src/routes/ambiente.routes.ts
@@ -7,8 +7,6 @@ router.get('/', AmbienteController.getAll)
 router.post('/', AmbienteController.create)
 router.put('/:id', AmbienteController.update)
 router.delete('/:id', AmbienteController.remove)
-router.get('/tipo/:tipo', AmbienteController.getByTipo)
-router.get('/habilitados', AmbienteController.getHabilitados)
 router.get('/:id', AmbienteController.getById)
 
 export default router
